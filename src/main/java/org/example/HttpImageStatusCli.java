8b97b1771164d2fae6f7c0cb9a50099204db6baa
Roman Belnitskiy <roman.belnitskiy@gmail.com>
2023-10-22 22:35:30 +0300

    added while loop rof cli

--- src/main/java/org/example/HttpImageStatusCli.java
+++ src/main/java/org/example/HttpImageStatusCli.java
@@ -5,20 +5,25 @@ import java.util.Scanner;
 public class HttpImageStatusCli {
 
     public static void askStatus() {
-        System.out.println("Enter HTTP status code");
-        String stringCode = new Scanner(System.in).nextLine();
+        while(true) {
+            System.out.println("Enter HTTP status code or \'0\' for exit");
+            String stringCode = new Scanner(System.in).nextLine();
 
-        try {
-            int code = Integer.parseInt(stringCode);
+            try {
+                int code = Integer.parseInt(stringCode);
+                if (code == 0) {
+                    return;
+                }
 
-            HttpStatusImageDownloader downloader = new HttpStatusImageDownloader();
-            downloader.downloadStatusImage(code);
+                HttpStatusImageDownloader downloader = new HttpStatusImageDownloader();
+                downloader.downloadStatusImage(code);
 
-            System.out.println("Image has been downloaded");
-        } catch (NumberFormatException ex) {
-            System.out.println("Please enter valid number");
-        } catch (RuntimeException ex) {
-            System.out.println(String.format("There is not image for HTTP status %s", stringCode));
+                System.out.println("Image has been downloaded");
+            } catch (NumberFormatException ex) {
+                System.out.println("Please enter valid number");
+            } catch (RuntimeException ex) {
+                System.out.println(String.format("There is not image for HTTP status %s", stringCode));
+            }
         }
     }
 }
